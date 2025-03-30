e126dab83c48a565e8736ce3616f17dcbce52f7b
quinise <quinise.ercolano@gmail.com>
2025-03-30 12:54:45 -0700

    initial commit

---
+++ src/main/java/com/khepraptah/khepra_backend/KhepraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.khepraptah.khepra_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhepraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KhepraBackendApplication.class, args);
+	}
+
+}
