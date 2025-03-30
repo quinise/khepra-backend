654802a45ba0f7032994a0e413cae8c756727000
quinise <quinise.ercolano@gmail.com>
2025-03-30 12:54:45 -0700

    initial commit

---
+++ src/test/java/com/khepraptah/khepra_backend/KhepraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khepraptah.khepra_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhepraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
