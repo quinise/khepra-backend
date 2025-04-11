721cceb56db03742eaf5b0b6a342507acd3bdfe7
quinise <quinise.ercolano@gmail.com>
2025-04-11 11:13:40 -0700

    Integrated Angular frontend with Spring Boot backend

--- src/main/java/com/khepraptah/khepra_site_backend/model/AppointmentDTO.java
+++ src/main/java/com/khepraptah/khepra_site_backend/model/AppointmentDTO.java
@@ -2,6 +2,5 @@ package com.khepraptah.khepra_site_backend.model;
 
 import java.util.Date;
 
-// Definitions: DTO means Data Transfer Object, DAO Data Access Object
-public record AppointmentDTO(Long id, String type, String name, String email, double phone_number, Date date, boolean isVirtual) {
+public record AppointmentDTO(Long id, String type, String name, String email, double phone_number, Date date, Boolean isVirtual) {
 }
