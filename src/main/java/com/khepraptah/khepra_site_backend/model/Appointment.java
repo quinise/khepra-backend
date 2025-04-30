95f295f46caeb936e24b2d65f2ed362179215ef7
quinise <quinise.ercolano@gmail.com>
2025-04-30 12:02:18 -0700

    Added userId to Appointment requiremnts

--- src/main/java/com/khepraptah/khepra_site_backend/model/Appointment.java
+++ src/main/java/com/khepraptah/khepra_site_backend/model/Appointment.java
@@ -16,6 +16,9 @@ public class Appointment {
     private Long id;
     @Column(name = "appointment_type")
     private String type;
+
+    @Column(name = "user_id")
+    private String userId;
     @Column(name = "client_name")
     private String name;
 
@@ -35,6 +38,9 @@ public class Appointment {
 
     public String getType() { return type; }
     public void setType(String type) { this.type = type; }
+
+    public String getUserId() { return userId; }
+    public void setUserId(String userId) { this.userId = userId; }
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
