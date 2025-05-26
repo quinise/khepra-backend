efcc49daa71da3d48d4c3f81a7e190c51639f673
quinise <quinise.ercolano@gmail.com>
2025-05-25 18:42:36 -0700

    Implemented pessimistic lock with buffers

--- src/main/java/com/khepraptah/khepra_site_backend/service/EventServiceImpl.java
+++ src/main/java/com/khepraptah/khepra_site_backend/service/EventServiceImpl.java
@@ -2,19 +2,25 @@ package com.khepraptah.khepra_site_backend.service;
 
 import com.khepraptah.khepra_site_backend.model.Event;
 import com.khepraptah.khepra_site_backend.model.EventDTO;
+import com.khepraptah.khepra_site_backend.model.EventType;
 import com.khepraptah.khepra_site_backend.repository.EventRepository;
+import com.khepraptah.khepra_site_backend.service.ScheduleConflictService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 class EventServiceImpl implements EventService {
     private final EventRepository eventRepository;
+    private final ScheduleConflictService scheduleConflictService;
 
-    public EventServiceImpl(EventRepository eventRepository) {
+    public EventServiceImpl(EventRepository eventRepository, ScheduleConflictService scheduleConflictService) {
         this.eventRepository = eventRepository;
+        this.scheduleConflictService = scheduleConflictService;
     }
 
     @Override
@@ -29,27 +35,41 @@ class EventServiceImpl implements EventService {
         return eventRepository.findById(id).map(this::convertToDTO);
     }
 
-    @Override
+    @Transactional
     public EventDTO saveEvent(EventDTO eventDTO) {
         Event event = convertToEntity(eventDTO);
+        event.calculateEndTime();
+        scheduleConflictService.checkForConflicts(event);
         Event savedEvent = eventRepository.save(event);
         return convertToDTO(savedEvent);
     }
 
-    @Override
+    @Transactional
     public EventDTO updateEvent(Long id, EventDTO eventDTO) {
         Event event = eventRepository.findById(id).orElseThrow();
         event.setEventName(eventDTO.eventName());
         event.setEventType(eventDTO.eventType());
         event.setClientName(eventDTO.clientName());
         event.setStartDate(eventDTO.startDate());
-        event.setEndDate(eventDTO.endDate());
+        event.setStartTime(eventDTO.startTime());
+
+        try {
+            EventType appointmentType =EventType.valueOf(eventDTO.eventType().toUpperCase());
+            event.setDuration(appointmentType.getDurationMinutes());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Invalid appointment type: " + eventDTO.eventType());
+        }
+
+        event.setEndTime(eventDTO.endTime());
         event.setStreetAddress(eventDTO.streetAddress());
         event.setCity(eventDTO.city());
         event.setState(eventDTO.state());
         event.setZipCode(eventDTO.zipCode());
         event.setDescription(eventDTO.description());
         event.setIsVirtual(eventDTO.isVirtual());
+
+        scheduleConflictService.checkForConflicts(event);
+
         Event updateEvent = eventRepository.save(event);
         return convertToDTO(updateEvent);
     }
@@ -60,16 +80,35 @@ class EventServiceImpl implements EventService {
     }
 
     private EventDTO convertToDTO(Event event) {
-        return new EventDTO(event.getId(), event.getEventName(), event.getEventType(), event.getClientName(), event.getStartDate(), event.getEndDate(), event.getStreetAddress(), event.getCity(), event.getState(), event.getZipCode(), event.getDescription(), event.getIsVirtual());
+        return new EventDTO(
+                event.getId(),
+                event.getEventName(),
+                event.getEventType(),
+                event.getClientName(),
+                event.getStartDate(),
+                event.getEndDate(),
+                event.getDuration(),
+                event.getStartTime(),
+                event.getEndTime(),
+                event.getStreetAddress(),
+                event.getCity(),
+                event.getState(),
+                event.getZipCode(),
+                event.getDescription(),
+                event.getIsVirtualRaw()
+        );
     }
 
     private Event convertToEntity(EventDTO eventDTO) {
         Event event = new Event();
+        event.setId(eventDTO.id());
         event.setEventName(eventDTO.eventName());
         event.setEventType(eventDTO.eventType());
         event.setClientName(eventDTO.clientName());
         event.setStartDate(eventDTO.startDate());
-        event.setEndDate(eventDTO.endDate());
+        event.setDuration(eventDTO.durationMinutes());
+        event.setStartTime(eventDTO.startTime());
+        event.setEndTime(eventDTO.endTime());
         event.setStreetAddress(eventDTO.streetAddress());
         event.setCity(eventDTO.city());
         event.setState(eventDTO.state());
